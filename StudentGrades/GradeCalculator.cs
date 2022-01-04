f78d31bf816cc4db592876604a07d5a9c9bcdfd5
Curious Drive <CuriousDrive@users.noreply.github.com>
2022-01-04 08:07:20 -0500

    Initial Commit

---
+++ StudentGrades/GradeCalculator.cs
@@ -0,0 +1,21 @@
+﻿namespace StudentGrades
+{
+    public class GradeCalculator
+    {
+        public string GetGradeByScore(int percentage)
+        {
+            if (percentage >= 90 & percentage <= 100)
+                return "A";
+            else if (percentage >= 80 && percentage < 90)
+                return "B";
+            else if (percentage >= 70 && percentage < 80)
+                return "C";
+            else if (percentage >= 60 && percentage < 70)
+                return "D";
+            else if (percentage >= 0 && percentage < 60)
+                return "F";
+            else
+                return "Invalid";
+        }
+    }
+}
---
+++ StudentGrades/Program.cs
@@ -0,0 +1,12 @@
+﻿using StudentGrades;
+
+// Get percentage
+var gradeCalculator = new GradeCalculator();
+
+Console.Write("Enter the percentage : ");
+var percentage = Convert.ToInt32(Console.ReadLine());
+
+var grade = gradeCalculator.GetGradeByScore(percentage);
+
+// Print grade
+Console.WriteLine($"Student Grade : {grade}");
